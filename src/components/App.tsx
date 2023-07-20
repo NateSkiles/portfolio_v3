5fe1ac730b36efa9abb0abba947f33d842669f77
Nathan Skiles <nateskiles94@gmail.com>
2023-07-20 09:45:39 -0700

    init

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="relative overflow-hidden bg-slate-900">
+      <div className="h-screen sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
+        Hello World
+      </div>
+    </div>
+  )
+}
+
+export default App
