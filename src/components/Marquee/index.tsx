c094547c4d1836b0bbe999633a9c049381b2906d
Nathan Skiles <nateskiles94@gmail.com>
2023-08-23 16:48:44 -0700

    add marquee component

---
+++ src/components/Marquee/index.tsx
@@ -0,0 +1,63 @@
+import {
+  SiBootstrap,
+  SiCsharp,
+  SiCss3,
+  SiDjango,
+  SiDotnet,
+  SiGit,
+  SiGnubash,
+  SiHtml5,
+  SiJavascript,
+  SiJest,
+  SiMongodb,
+  SiMysql,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiPython,
+  SiReact,
+  SiVercel
+} from 'react-icons/si'
+
+export default function Marquee() {
+  return (
+    <div className="marquee pt-12">
+      <div className="marquee-inner inline-flex">
+        <SiJavascript size={128} color="#F7DF1E" className="mx-5" />
+        <SiReact size={128} color="#61DAFB" className="mx-5" />
+        <SiNodedotjs size={128} color="#339933" className="mx-5" />
+        <SiNextdotjs size={128} color="#000000" className="mx-5" />
+        <SiVercel size={128} color="#000000" className="mx-5" />
+        <SiJest size={128} color="#C21325" className="mx-5" />
+        <SiHtml5 size={128} color="#E34F26" className="mx-5" />
+        <SiCss3 size={128} color="#1572B6" className="mx-5" />
+        <SiBootstrap size={128} color="#7952B3" className="mx-5" />
+        <SiCsharp size={128} color="#239120" className="mx-5" />
+        <SiDotnet size={128} color="#512BD4" className="mx-5" />
+        <SiPython size={128} color="#3776AB" className="mx-5" />
+        <SiDjango size={128} color="#092E20" className="mx-10" />
+        <SiGnubash size={128} color="#4EAA25" className="mx-5" />
+        <SiGit size={128} color="#F05032" className="mx-5" />
+        <SiMysql size={128} color="#4479A1" className="mx-5" />
+        <SiMongodb size={128} color="#47A248" className="mx-5" />
+
+        <SiJavascript size={128} color="#F7DF1E" className="mx-5" />
+        <SiReact size={128} color="#61DAFB" className="mx-5" />
+        <SiNodedotjs size={128} color="#339933" className="mx-5" />
+        <SiNextdotjs size={128} color="#000000" className="mx-5" />
+        <SiVercel size={128} color="#000000" className="mx-5" />
+        <SiJest size={128} color="#C21325" className="mx-5" />
+        <SiHtml5 size={128} color="#E34F26" className="mx-5" />
+        <SiCss3 size={128} color="#1572B6" className="mx-5" />
+        <SiBootstrap size={128} color="#7952B3" className="mx-5" />
+        <SiCsharp size={128} color="#239120" className="mx-5" />
+        <SiDotnet size={128} color="#512BD4" className="mx-5" />
+        <SiPython size={128} color="#3776AB" className="mx-5" />
+        <SiDjango size={128} color="#092E20" className="mx-10" />
+        <SiGnubash size={128} color="#4EAA25" className="mx-5" />
+        <SiGit size={128} color="#F05032" className="mx-5" />
+        <SiMysql size={128} color="#4479A1" className="mx-5" />
+        <SiMongodb size={128} color="#47A248" className="mx-5" />
+      </div>
+    </div>
+  )
+}
--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,22 +1,4 @@
-import {
-  SiBootstrap,
-  SiCsharp,
-  SiCss3,
-  SiDjango,
-  SiDotnet,
-  SiGit,
-  SiGnubash,
-  SiHtml5,
-  SiJavascript,
-  SiJest,
-  SiMongodb,
-  SiMysql,
-  SiNextdotjs,
-  SiNodedotjs,
-  SiPython,
-  SiReact,
-  SiVercel
-} from 'react-icons/si'
+import Marquee from 'components/Marquee'
 
 export default function Skills() {
   return (
@@ -26,45 +8,7 @@ export default function Skills() {
           Technologies I Work With
         </h2>
 
-        <div className="marquee pt-12">
-          <div className="marquee-inner inline-flex">
-            <SiJavascript size={128} color="#F7DF1E" className="mx-5" />
-            <SiReact size={128} color="#61DAFB" className="mx-5" />
-            <SiNodedotjs size={128} color="#339933" className="mx-5" />
-            <SiNextdotjs size={128} color="#000000" className="mx-5" />
-            <SiVercel size={128} color="#000000" className="mx-5" />
-            <SiJest size={128} color="#C21325" className="mx-5" />
-            <SiHtml5 size={128} color="#E34F26" className="mx-5" />
-            <SiCss3 size={128} color="#1572B6" className="mx-5" />
-            <SiBootstrap size={128} color="#7952B3" className="mx-5" />
-            <SiCsharp size={128} color="#239120" className="mx-5" />
-            <SiDotnet size={128} color="#512BD4" className="mx-5" />
-            <SiPython size={128} color="#3776AB" className="mx-5" />
-            <SiDjango size={128} color="#092E20" className="mx-10" />
-            <SiGnubash size={128} color="#4EAA25" className="mx-5" />
-            <SiGit size={128} color="#F05032" className="mx-5" />
-            <SiMysql size={128} color="#4479A1" className="mx-5" />
-            <SiMongodb size={128} color="#47A248" className="mx-5" />
-
-            <SiJavascript size={128} color="#F7DF1E" className="mx-5" />
-            <SiReact size={128} color="#61DAFB" className="mx-5" />
-            <SiNodedotjs size={128} color="#339933" className="mx-5" />
-            <SiNextdotjs size={128} color="#000000" className="mx-5" />
-            <SiVercel size={128} color="#000000" className="mx-5" />
-            <SiJest size={128} color="#C21325" className="mx-5" />
-            <SiHtml5 size={128} color="#E34F26" className="mx-5" />
-            <SiCss3 size={128} color="#1572B6" className="mx-5" />
-            <SiBootstrap size={128} color="#7952B3" className="mx-5" />
-            <SiCsharp size={128} color="#239120" className="mx-5" />
-            <SiDotnet size={128} color="#512BD4" className="mx-5" />
-            <SiPython size={128} color="#3776AB" className="mx-5" />
-            <SiDjango size={128} color="#092E20" className="mx-10" />
-            <SiGnubash size={128} color="#4EAA25" className="mx-5" />
-            <SiGit size={128} color="#F05032" className="mx-5" />
-            <SiMysql size={128} color="#4479A1" className="mx-5" />
-            <SiMongodb size={128} color="#47A248" className="mx-5" />
-          </div>
-        </div>
+        <Marquee />
       </div>
     </div>
   )
