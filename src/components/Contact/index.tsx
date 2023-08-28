19fbe93a254a4b6d917f7846c559d1cc7a3fd813
Nathan Skiles <nateskiles94@gmail.com>
2023-08-28 11:47:09 -0700

    add formspree, remove servless function (cors issues)

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,14 +1,44 @@
+import React, { useState } from 'react'
+
 export default function Contact() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [msg, setMsg] = useState('')
+
+  const formSubmit = () => {
+    const requestOptions: RequestInit = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ name: name, email: email, message: msg })
+    }
+    fetch('https://formspree.io/f/mpzgdevk', requestOptions)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err))
+  }
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value)
+  }
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value)
+  }
+  const handleMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMsg(e.target.value)
+  }
+
   return (
     <div>
       <section id="contact" className="relative bg-gray-900 text-slate-100">
         <div className="container mx-auto px-5 py-24">
           <div className="mb-12 flex w-full flex-col text-center">
             <h1 className="mb-4 text-3xl font-medium text-slate-100 sm:text-3xl">
-              Contact Me
+              Contact
             </h1>
             <p className="mx-auto text-base leading-relaxed lg:w-2/3">
-              Reach out to me below!
+              Send me a message:
             </p>
           </div>
           <div className="mx-auto md:w-2/3 lg:w-1/2">
@@ -25,6 +55,7 @@ export default function Contact() {
                     type="text"
                     id="name"
                     name="name"
+                    onChange={handleNameChange}
                     className="w-full rounded border border-gray-300 bg-gray-100 px-3 py-1 text-base leading-8 text-slate-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500"
                   />
                 </div>
@@ -41,6 +72,7 @@ export default function Contact() {
                     type="email"
                     id="email"
                     name="email"
+                    onChange={handleEmailChange}
                     className="w-full rounded border border-gray-300 bg-gray-100 px-3 py-1 text-base leading-8 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500"
                   />
                 </div>
@@ -56,13 +88,17 @@ export default function Contact() {
                   <textarea
                     id="message"
                     name="message"
+                    onChange={handleMessageChange}
                     className="h-32 w-full resize-none rounded border border-gray-300 bg-gray-100 px-3 py-1 text-base leading-6 text-gray-700 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500"
                   ></textarea>
                 </div>
               </div>
               <div className="w-full p-2">
-                <button className="mx-auto flex rounded border-0 bg-blue-600 px-8 py-2 text-lg text-white hover:bg-blue-800 focus:outline-none">
-                  Button
+                <button
+                  onClick={formSubmit}
+                  className="mx-auto flex rounded border-0 bg-blue-600 px-8 py-2 text-lg text-white hover:bg-blue-800 focus:outline-none"
+                >
+                  Submit
                 </button>
               </div>
               <div className="mt-8 w-full border-t border-gray-200 p-2 pt-8 text-center">
